875dbddcdd81d9d6ecf8375bdf33cc3ebbf05fb0
Antonio Borrero Granell <abogra1987@gmail.com>
2013-10-17 19:13:29 +0200

    Finish 11.2.4. Correct some bugs in User Controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController  
-  before_action :signed_in_user,      only: [:index, :edit, :update]
+  before_action :signed_in_user,
+                 only: [:index, :edit, :update, :destroy, :following, :followers]  
   before_action :correct_user,        only: [:edit, :update]
   before_action :admin_user,          only: :destroy
   before_action :already_registered,  only: [:new, :create]
@@ -56,7 +57,7 @@ class UsersController < ApplicationController
   def followers
     @title = "Followers"
     @user = User.find(params[:id])
-    @users = @user.followed_users.paginate(page: params[:page])
+    @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
   end
 
